9fb0fe06faf66a9b55fbbdd70d3436b481226664
Harysson <haryssonsoares@gmail.com>
2023-01-08 21:46:20 -0300

    feat: input number cell mask

--- src/components/domain/signUp/index.tsx
+++ src/components/domain/signUp/index.tsx
@@ -1,14 +1,19 @@
 import { useRouter } from "next/router";
 import React from "react";
 import { Button, Card, Form } from "react-bootstrap";
-import { useWindowSize } from "../../../Hooks/windowsize";
+import InputMask from "react-input-mask";
+
+import listAddress from "./utils/address";
 
 const SignUp = () => {
 	const route = useRouter();
+
+	const { houses, squares } = listAddress();
+
 	return (
 		<div className="d-flex h-100vh">
 			<Button
-				className="position-absolute t-25px l-25px d-flex justify-content-center align-items-end"
+				className="position-absolute t-15px l-15px d-flex justify-content-center align-items-end"
 				onClick={() => route.back()}
 			>
 				<i className="fs-1 fa fa-angle-left me-2"></i>Voltar
@@ -19,16 +24,30 @@ const SignUp = () => {
 						<Form>
 							<Form.Group className="mb-3">
 								<Form.Label>Telefone</Form.Label>
-								<Form.Control type="number" placeholder="Digite seu telefone" />
+								<InputMask
+									className="form-control"
+									mask="+55(099)9999-9999"
+									maskChar=" "
+								/>
 							</Form.Group>
 							<Form.Group className="mb-3">
 								<Form.Label>Endereço</Form.Label>
-								<Form.Select aria-label="Default select example">
-									<option>Quadra</option>
-									[1,]
-									<option value="1">One</option>
-									<option value="2">Two</option>
-									<option value="3">Three</option>
+								<Form.Select
+									aria-label="What's your square house address"
+									className="mb-3"
+								>
+									{squares.map((square, index) => (
+										<option key={index} value={index + 1}>
+											Quadra {index + 1}
+										</option>
+									))}
+								</Form.Select>
+								<Form.Select aria-label="What's your house number address">
+									{houses.map((house, index) => (
+										<option key={index} value={index + 1}>
+											Casa {index + 1}
+										</option>
+									))}
 								</Form.Select>
 							</Form.Group>
 							<Form.Group className="mb-3">
---
+++ src/components/domain/signUp/utils/address.ts
@@ -0,0 +1,8 @@
+const listAddress = () => {
+	const squares = new Array(19).fill(0);
+	const houses = new Array(100).fill(0);
+
+	return { squares, houses };
+};
+
+export default listAddress;
