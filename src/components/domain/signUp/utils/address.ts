238cd4cba2e3db91024d5e99da542a0a192c632a
Harysson <haryssonsoares@gmail.com>
2023-01-08 22:03:16 -0300

    fix: number houses address

--- src/components/domain/signUp/utils/address.ts
+++ src/components/domain/signUp/utils/address.ts
@@ -1,6 +1,6 @@
 const listAddress = () => {
 	const squares = new Array(19).fill(0);
-	const houses = new Array(100).fill(0);
+	const houses = new Array(55).fill(0);
 
 	return { squares, houses };
 };
